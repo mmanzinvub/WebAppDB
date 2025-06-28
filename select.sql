e49e48020be55c83ab951f0b1b922bd6467053be
mmanzinvub <150946687+mmanzinvub@users.noreply.github.com>
2025-06-28 19:21:27 +0200

    Update select.sql

--- select.sql
+++ select.sql
@@ -1,7 +1,21 @@
-SELECT * FROM table_cameras;
-SELECT * FROM table_lenses;
-SELECT * FROM table_equipment;
-SELECT * FROM table_films;
-SELECT * FROM table_customers;
-SELECT * FROM table_orders;
-SELECT * FROM table_order_items;
+-- prikaz svih dostupnih kamera
+SELECT camera_id, name, price, stock
+FROM table_cameras
+WHERE stock > 0;
+
+-- prikaz objektiva koji su ispod ili 135 eura, silazno
+SELECT lens_id, name, aperture, price, stock
+FROM table_lenses
+WHERE price <= 135
+ORDER BY PRICE DESC;
+
+-- prikaz svih dostupnih bjeskalica
+SELECT TABLE_EQUIPMENT_ID, name, price, stock
+FROM TABLE_EQUIPMENT
+WHERE CATEGORY = 'Bjeskalica' AND stock > 0;
+
+-- prikaz svih filmova koji su ISO 400
+SELECT film_id, name, price, stock
+FROM TABLE_FILMS
+WHERE NAME LIKE '% 400';
+
