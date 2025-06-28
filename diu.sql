bd000cf0ddba644b7156da1d41143ff12838a987
mmanzinvub <150946687+mmanzinvub@users.noreply.github.com>
2025-06-28 17:22:51 +0200

    Update diu.sql

--- diu.sql
+++ diu.sql
@@ -1,60 +1,125 @@
 -- INSERT
-
+-- cameras insert
 INSERT INTO table_cameras (IMAGE_URL, CATEGORY, NAME, PRICE, STOCK)
 VALUES ('img/minolta-srt-101.png', 'SLR', 'Minolta SRT 101', 180.00, 0);
+
 INSERT INTO table_cameras (IMAGE_URL, CATEGORY, NAME, PRICE, STOCK)
 VALUES ('img/minolta-xe1.png', 'SLR', 'Minolta XE1', 180.00, 0);
+
 INSERT INTO table_cameras (IMAGE_URL, CATEGORY, NAME, PRICE, STOCK)
 VALUES ('img/canon-ae-1.png', 'SLR', 'Canon AE1', 200.00, 0);
+
 INSERT INTO table_cameras (IMAGE_URL, CATEGORY, NAME, PRICE, STOCK)
 VALUES ('img/canon-av-1.png', 'SLR', 'Canon AV1', 150.00, 0);
+
 INSERT INTO table_cameras (IMAGE_URL, CATEGORY, NAME, PRICE, STOCK)
 VALUES ('img/yashica-tl-electro-x.png', 'SLR', 'Yashica TL Electro X', 130.00, 0);
+
 INSERT INTO table_cameras (IMAGE_URL, CATEGORY, NAME, PRICE, STOCK)
 VALUES ('img/yashica-electro35-gsn.png', 'Rangefinder', 'Yashica Electro 35 GSN', 125.00, 0);
+
 INSERT INTO table_cameras (IMAGE_URL, CATEGORY, NAME, PRICE, STOCK)
 VALUES ('img/olympus-xa2.png', 'Point & Shoot', 'Olympus XA2', 80.00, 0);
+
 INSERT INTO table_cameras (IMAGE_URL, CATEGORY, NAME, PRICE, STOCK)
 VALUES ('img/pentax-67.png', 'Medium Format', 'Pentax 67', 750.00, 0);
+
 INSERT INTO table_cameras (IMAGE_URL, CATEGORY, NAME, PRICE, STOCK)
 VALUES ('img/polaroid-supercolor1000.png', 'Instant Camera', 'Polaroid Supercolor 1000', 75.00, 0);
+
 INSERT INTO table_cameras (IMAGE_URL, CATEGORY, NAME, PRICE, STOCK)
 VALUES ('img/fujifilm-finepix-jv100.png', 'Digital Camera', 'Fujifilm FinePix JV100', 69.00, 0);
 
+-- lenses insert
 INSERT INTO table_lenses (IMAGE_URL, CATEGORY, APERTURE, NAME, PRICE, STOCK)
 VALUES ('img/minolta-md-rokkor-28mm-f28.png', 'Wide', '28mm f2.8', 'Minolta MD Rokkor', 90.00, 0);
+
 INSERT INTO table_lenses (IMAGE_URL, CATEGORY, APERTURE, NAME, PRICE, STOCK)
 VALUES ('img/minolta-md-rokkor-50mm-f14.png', 'Standard', '50mm f1.4', 'Minolta MD Rokkor', 100.00, 0);
+
 INSERT INTO table_lenses (IMAGE_URL, CATEGORY, APERTURE, NAME, PRICE, STOCK)
 VALUES ('img/canon-ef-28-105mm-f35-45.png', 'Wide / Standard / Telephoto', '28-105mm f3.5-4.5', 'Canon EF Ultrasonic', 120.00, 0);
+
 INSERT INTO table_lenses (IMAGE_URL, CATEGORY, APERTURE, NAME, PRICE, STOCK)
 VALUES ('img/canon-ef-50mm-f18.png', 'Standard', '50mm f1.8', 'Canon EF', 125.00, 0);
+
 INSERT INTO table_lenses (IMAGE_URL, CATEGORY, APERTURE, NAME, PRICE, STOCK)
 VALUES ('img/nikon-nikkor-43-86mm-f35.png', 'Standard / Telephoto', '43-86mm f3.5', 'Nikon NIKKOR', 150.00, 0);
+
 INSERT INTO table_lenses (IMAGE_URL, CATEGORY, APERTURE, NAME, PRICE, STOCK)
 VALUES ('img/revuenon-auto-mc-135mm-f28.png', 'Telephoto', '135mm f2.8', 'Revuenon Auto MC', 150.00, 0);
 
+INSERT INTO table_lenses (IMAGE_URL, CATEGORY, APERTURE, NAME, PRICE, STOCK)
+VALUES ('img/minolta-md-135mm-f35.png', 'Telephoto', '135mm f3.5', 'Minolta MD Tele Rokkor', 110.00, 0);
+
+INSERT INTO table_lenses (IMAGE_URL, CATEGORY, APERTURE, NAME, PRICE, STOCK)
+VALUES ('img/pentax-smc-50mm-f14.png', 'Standard', '50mm f1.4', 'Pentax SMC 50mm', 130.00, 0);
+
+INSERT INTO table_lenses (IMAGE_URL, CATEGORY, APERTURE, NAME, PRICE, STOCK)
+VALUES ('img/olympus-zuiko-28mm-f35.png', 'Wide', '28mm f3.5', 'Olympus Zuiko Wide', 95.00, 0);
+
+INSERT INTO table_lenses (IMAGE_URL, CATEGORY, APERTURE, NAME, PRICE, STOCK)
+VALUES ('img/yashica-50mm-f18.png', 'Standard', '50mm f1.8', 'Yashica ML 50mm', 85.00, 0);
+
+-- equipment insert
 INSERT INTO table_equipment (IMAGE_URL, CATEGORY, NAME, PRICE, STOCK)
 VALUES ('img/vivitar283.png', 'Bjeskalica', 'Vivitar 283', 50.00, 0);
+
 INSERT INTO table_equipment (IMAGE_URL, CATEGORY, NAME, PRICE, STOCK)
 VALUES ('img/vivitar285.png', 'Bjeskalica', 'Vivitar 285', 70.00, 0);
+
 INSERT INTO table_equipment (IMAGE_URL, CATEGORY, NAME, PRICE, STOCK)
 VALUES ('img/cullmann-alpha-stativ.png', 'Stativ', 'Cullmann Alpha 1800', 40.00, 0);
+
 INSERT INTO table_equipment (IMAGE_URL, CATEGORY, NAME, PRICE, STOCK)
 VALUES ('img/shutter-release-cable.png', 'Okidac', 'Okidac za fotoaparat 1m', 10.00, 0);
+
 INSERT INTO table_equipment (IMAGE_URL, CATEGORY, NAME, PRICE, STOCK)
 VALUES ('img/lomography-scanner.png', 'Skener', 'Lomography skener za 35mm film', 40.00, 0);
 
+INSERT INTO table_equipment (IMAGE_URL, CATEGORY, NAME, PRICE, STOCK)
+VALUES ('img/yongnuo-yn560iii.png', 'Bjeskalica', 'Yongnuo YN560III', 60.00, 0);
+
+INSERT INTO table_equipment (IMAGE_URL, CATEGORY, NAME, PRICE, STOCK)
+VALUES ('img/manfrotto-tripod.png', 'Stativ', 'Manfrotto Compact Tripod', 85.00, 0);
+
+INSERT INTO table_equipment (IMAGE_URL, CATEGORY, NAME, PRICE, STOCK)
+VALUES ('img/hoya-uv-filter.png', 'Filter', 'Hoya UV Filter 52mm', 18.00, 0);
+
+INSERT INTO table_equipment (IMAGE_URL, CATEGORY, NAME, PRICE, STOCK)
+VALUES ('img/paterson-tank.png', 'Razvijac', 'Paterson Developing Tank', 35.00, 0);
+
+INSERT INTO table_equipment (IMAGE_URL, CATEGORY, NAME, PRICE, STOCK)
+VALUES ('img/energizer-aa-battery.png', 'Baterija', 'Energizer AA 4-pack', 7.00, 0);
+
+-- films insert
 INSERT INTO table_films (IMAGE_URL, CATEGORY, NAME, PRICE, STOCK)
 VALUES ('img/kodak-gold-200-36-exp.png', '35mm', 'Kodak Gold 200', 15.00, 0);
+
 INSERT INTO table_films (IMAGE_URL, CATEGORY, NAME, PRICE, STOCK)
 VALUES ('img/agfa-apx-100-36-exp.png', '35mm', 'Agfa APX 100', 12.00, 0);
+
 INSERT INTO table_films (IMAGE_URL, CATEGORY, NAME, PRICE, STOCK)
 VALUES ('img/candido400.png', '35mm', 'Candido 400', 17.00, 0);
+
 INSERT INTO table_films (IMAGE_URL, CATEGORY, NAME, PRICE, STOCK)
 VALUES ('img/candido800.png', '35mm', 'Candido 800', 20.00, 0);
+
 INSERT INTO table_films (IMAGE_URL, CATEGORY, NAME, PRICE, STOCK)
 VALUES ('img/wolfen-nc500.png', '35mm', 'Wolfen NC 500', 25.00, 0);
+
+INSERT INTO table_films (IMAGE_URL, CATEGORY, NAME, PRICE, STOCK)
+VALUES ('img/ilford-hp5.png', '35mm', 'Ilford HP5 Plus 400', 16.00, 0);
+
+INSERT INTO table_films (IMAGE_URL, CATEGORY, NAME, PRICE, STOCK)
+VALUES ('img/fujifilm-superia.png', '35mm', 'Fujifilm Superia X-TRA 400', 14.00, 0);
+
+INSERT INTO table_films (IMAGE_URL, CATEGORY, NAME, PRICE, STOCK)
+VALUES ('img/kodak-portra.png', '120mm', 'Kodak Portra 160', 22.00, 0);
+
+INSERT INTO table_films (IMAGE_URL, CATEGORY, NAME, PRICE, STOCK)
+VALUES ('img/ilford-delta.png', '120mm', 'Ilford Delta 3200', 24.00, 0);
+
 INSERT INTO table_films (IMAGE_URL, CATEGORY, NAME, PRICE, STOCK)
 VALUES ('img/polaroid-color600.png', 'Instant Film', 'Polaroid Color 600', 30.00, 0);
 
@@ -88,6 +153,18 @@ UPDATE table_cameras
 SET STOCK = 1
 WHERE CATEGORY = 'Medium Format';
 
+UPDATE table_cameras
+SET PRICE = 185.00
+WHERE NAME = 'Minolta SRT 101';
+
+UPDATE table_cameras
+SET STOCK = 8
+WHERE NAME = 'Canon AV1';
+
+UPDATE table_cameras
+SET PRICE = 135.00
+WHERE CATEGORY = 'Rangefinder';
+
 -- lenses
 UPDATE table_lenses
 SET PRICE = 112.00, STOCK = 12
@@ -113,6 +190,22 @@ UPDATE table_lenses
 SET PRICE = 170.00, STOCK = 10
 WHERE CATEGORY = 'Telephoto';
 
+UPDATE table_lenses
+SET STOCK = 18
+WHERE NAME = 'Pentax SMC 50mm';
+
+UPDATE table_lenses
+SET PRICE = 120.00
+WHERE CATEGORY = 'Wide';
+
+UPDATE table_lenses
+SET STOCK = 9
+WHERE NAME = 'Olympus Zuiko Wide';
+
+UPDATE table_lenses
+SET STOCK = 7
+WHERE NAME = 'Yashica ML 50mm';
+
 -- equipment
 UPDATE TABLE_EQUIPMENT
 SET STOCK = 20
@@ -129,6 +222,30 @@ WHERE PRICE = 40.00;
 UPDATE TABLE_EQUIPMENT
 SET STOCK = 150, PRICE = 15.00
 WHERE CATEGORY = 'Okidac';
+
+UPDATE table_equipment
+SET STOCK = 12
+WHERE NAME = 'Vivitar 285';
+
+UPDATE table_equipment
+SET PRICE = 45.00
+WHERE CATEGORY = 'Stativ';
+
+UPDATE table_equipment
+SET STOCK = 3
+WHERE NAME = 'Manfrotto Compact Tripod';
+
+UPDATE table_equipment
+SET PRICE = 20.00
+WHERE NAME = 'Hoya UV Filter 52mm';
+
+UPDATE table_equipment
+SET PRICE = 38.00
+WHERE NAME = 'Paterson Developing Tank';
+
+UPDATE table_equipment
+SET STOCK = 30
+WHERE CATEGORY = 'Baterija';
 
 -- films
 UPDATE TABLE_FILMS
@@ -155,4 +272,103 @@ UPDATE TABLE_FILMS
 SET STOCK = 120
 WHERE NAME = 'Wolfen NC 500' AND CATEGORY = '35mm';
 
+UPDATE table_films
+SET STOCK = 15
+WHERE NAME = 'Kodak Portra 160';
+
+UPDATE table_films
+SET PRICE = 26.00
+WHERE NAME = 'Wolfen NC 500';
+
+UPDATE table_films
+SET STOCK = 19
+WHERE NAME = 'Ilford HP5 Plus 400';
+
+UPDATE table_films
+SET PRICE = 16.00
+WHERE NAME = 'Fujifilm Superia X-TRA 400';
+
 -- DELETE
+-- cameras delete
+DELETE FROM table_cameras WHERE NAME = 'Fujifilm FinePix JV100';
+
+DELETE FROM table_cameras WHERE CATEGORY = 'Instant Camera';
+
+DELETE FROM table_cameras WHERE PRICE < 100;
+
+DELETE FROM table_cameras WHERE STOCK = 0 AND CATEGORY = 'Medium Format';
+
+DELETE FROM table_cameras WHERE NAME = 'Canon AV1';
+
+DELETE FROM table_cameras WHERE CATEGORY = 'Rangefinder';
+
+DELETE FROM table_cameras WHERE NAME LIKE '%Minolta%';
+
+DELETE FROM table_cameras WHERE STOCK = 1;
+
+DELETE FROM table_cameras WHERE CATEGORY = 'Digital Camera';
+
+DELETE FROM table_cameras WHERE NAME = 'Olympus XA2';
+
+-- lens delete
+DELETE FROM table_lenses WHERE NAME = 'Revuenon Auto MC';
+
+DELETE FROM table_lenses WHERE CATEGORY = 'Telephoto';
+
+DELETE FROM table_lenses WHERE STOCK = 0;
+
+DELETE FROM table_lenses WHERE PRICE > 140;
+
+DELETE FROM table_lenses WHERE NAME = 'Canon EF';
+
+DELETE FROM table_lenses WHERE APERTURE = '28mm f2.8';
+
+DELETE FROM table_lenses WHERE CATEGORY = 'Wide';
+
+DELETE FROM table_lenses WHERE NAME LIKE '%NIKKOR%';
+
+DELETE FROM table_lenses WHERE STOCK = 7;
+
+DELETE FROM table_lenses WHERE NAME = 'Olympus Zuiko Wide';
+
+-- equipment delete
+DELETE FROM table_equipment WHERE NAME = 'Vivitar 283';
+
+DELETE FROM table_equipment WHERE CATEGORY = 'Okidac';
+
+DELETE FROM table_equipment WHERE PRICE < 20;
+
+DELETE FROM table_equipment WHERE STOCK = 0 AND CATEGORY = 'Razvijac';
+
+DELETE FROM table_equipment WHERE NAME = 'Manfrotto Compact Tripod';
+
+DELETE FROM table_equipment WHERE NAME = 'Hoya UV Filter 52mm';
+
+DELETE FROM table_equipment WHERE CATEGORY = 'Bjeskalica';
+
+DELETE FROM table_equipment WHERE CATEGORY LIKE 'Baterija';
+
+DELETE FROM table_equipment WHERE STOCK = 3;
+
+DELETE FROM table_equipment WHERE NAME = 'Paterson Developing Tank';
+
+-- films delete
+DELETE FROM table_films WHERE NAME = 'Candido 400';
+
+DELETE FROM table_films WHERE CATEGORY = 'Instant Film';
+
+DELETE FROM table_films WHERE PRICE > 25;
+
+DELETE FROM table_films WHERE STOCK = 0 AND CATEGORY = '35mm';
+
+DELETE FROM table_films WHERE NAME = 'Wolfen NC 500';
+
+DELETE FROM table_films WHERE NAME = 'Kodak Portra 160';
+
+DELETE FROM table_films WHERE CATEGORY = '120mm';
+
+DELETE FROM table_films WHERE NAME LIKE '%Ilford%';
+
+DELETE FROM table_films WHERE STOCK = 19;
+
+DELETE FROM table_films WHERE PRICE < 16.00;
